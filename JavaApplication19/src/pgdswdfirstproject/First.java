f7061ad7b4ac3bc5e5d9b4d9d95eb8280e51e60b
pgdswd <pgdswd@lab-foss-pc>
2015-09-29 14:37:19 +0200

    Our Second commit

--- JavaApplication19/src/pgdswdfirstproject/First.java
+++ JavaApplication19/src/pgdswdfirstproject/First.java
@@ -13,5 +13,7 @@ public class First {
     public static void main(String[] args) {
     
       System.out.println("This is my first change");
+      
+      System.out.println("Another line of text");
   }
 }
