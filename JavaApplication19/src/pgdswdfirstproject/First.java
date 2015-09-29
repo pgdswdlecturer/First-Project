4cc9c56fe3336bc948201c7170d664a256b2a4c5
pgdswd <pgdswd@lab-foss-pc>
2015-09-29 14:32:09 +0200

    Our first commit

--- JavaApplication19/src/pgdswdfirstproject/First.java
+++ JavaApplication19/src/pgdswdfirstproject/First.java
@@ -10,5 +10,8 @@ package pgdswdfirstproject;
  * @author pgdswd
  */
 public class First {
+    public static void main(String[] args) {
     
+      System.out.println("This is my first change");
+  }
 }
